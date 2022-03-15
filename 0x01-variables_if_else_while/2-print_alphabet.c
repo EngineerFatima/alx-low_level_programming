60f8d7aef91739b5765a2864a1e430a7a0c1ae64
EngineerFatima <fatimazarahrabiu@gmail.com>
2022-03-14 18:18:35 -0700

    This is how to commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print last digit in random number
+ *
+ * Description: print last digit in random number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
